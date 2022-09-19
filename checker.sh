23493564c41970eee436a8d42a3b7d66a4ddb8a4
lalit wagh <68015501+dark-player@users.noreply.github.com>
2022-09-19 18:29:16 +0530

    Add files via upload

---
+++ checker.sh
@@ -0,0 +1,23 @@
+#bin/bash
+#scripted by lalit wagh
+# I am the real author
+# here are the colors and the codes
+red='\033[1;31m'
+rset='\033[0m'
+grn='\033[1;32m'
+ylo='\033[1;33m'
+blue='\033[1;34m'
+cyan='\033[1;36m'
+pink='\033[1;35m'
+
+#coding section starts here :)
+clear
+echo
+echo
+' what the fuck is this going on'
+|lolcat
+printf "\n"
+printf " Scripted by lalit"
+printf "\n"
+printf " subcribe my youtube channel"
+echo
